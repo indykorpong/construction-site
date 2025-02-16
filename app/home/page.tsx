6be8aabe890e4a6d4b11e05811299cbba7438737
Korpong Sawataksornchuen <korpong@skilllane.com>
2025-02-16 18:28:11 +0700

    feat: edit carousel component settings

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,9 @@
 'use client'
-import Image from 'next/image'
 
 import { Carousel } from '../components/carousel'
 import { useEffect } from 'react'
 import Swiper from 'swiper'
-import { Pagination, Navigation, Autoplay } from 'swiper/modules'
+import { Pagination, Navigation, Autoplay, Grid } from 'swiper/modules'
 
 export default function Home() {
   useEffect(() => {
@@ -12,7 +11,7 @@ export default function Home() {
     const swiper = new Swiper('.swiper', {
       loop: true,
       modules: [Pagination, Navigation, Autoplay],
-      slidesPerView: 1,
+      slidesPerView: 2,
       autoplay: {
         delay: 2500,
         disableOnInteraction: false,
@@ -31,36 +30,15 @@ export default function Home() {
   const home_preview = [1, 2, 3, 4, 5, 6].map((item, index) => {
     return (
       <div key={index} className="swiper-slide color-white">
-        <Image
-          src={`/home_preview/home_preview_0${item}.jpg`}
-          alt={`home_preview_0${item}`}
-          width={500}
-          height={500}
-          className="m-auto"
-        />
+        <img src={`/home_preview/home_preview_0${item}.jpg`} alt={`home_preview_0${item}`} className="m-auto" />
       </div>
     )
   })
 
-  const site_preview = [1, 2, 3, 4, 5, 6, 7, 8, 9].map((item, index) => (
-    <div key={index} className="swiper-slide color-white">
-      <Image
-        src={`/site_preview/site_preview_0${item}.jpg`}
-        alt={`site_preview_0${item}`}
-        width={500}
-        height={500}
-        className="m-auto"
-      />
-    </div>
-  ))
-
   return (
     <>
-      <div className="bg-gray-400">
+      <div className="bg-white">
         <Carousel>{home_preview}</Carousel>
-      </div>
-      <div className="bg-gray-600">
-        <Carousel>{site_preview}</Carousel>
       </div>
       <div>Home</div>
     </>
