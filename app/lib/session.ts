0d0e243e77bdad599b583bff1c5a3035765cd06b
Lam <109079862+LamLamLamXD@users.noreply.github.com>
2025-04-16 13:41:38 +0700

    change: redirect to admin if already login and login page retouch

--- app/lib/session.ts
+++ app/lib/session.ts
@@ -1,4 +1,4 @@
-import 'server-only'
+'use server'
 import { SignJWT, jwtVerify } from 'jose'
 import { SessionPayload } from '@/app/lib/definitions'
 import { cookies } from 'next/headers'
--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,7 @@ import { cookies } from 'next/headers'
 export default async function middleware(req: NextRequest) {
   // Check if the current route is protected
   const path = req.nextUrl.pathname
+  const loginPath = path.match(/login/i)
   const isProtectedRoute = path.startsWith('/admin')
 
   // Decrypt the session from the cookie
@@ -16,6 +17,11 @@ export default async function middleware(req: NextRequest) {
   // Redirect to /login if the user is not authenticated
   if (isProtectedRoute && !session?.id) {
     return NextResponse.redirect(new URL('/login', req.nextUrl))
+  }
+
+  // Redirect to /admin if /login while already authenticated
+  if (loginPath && session?.id) {
+    return NextResponse.redirect(new URL('/admin', req.nextUrl))
   }
 
   return NextResponse.next()
