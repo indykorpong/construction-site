6128cf4a51d09d067fefbd77d6d74a36b89f12d8
Lam <109079862+LamLamLamXD@users.noreply.github.com>
2025-06-02 20:37:40 +0700

    update: login file structure

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { decrypt } from '@/app/lib/session'
+import { decrypt } from '@/lib/login/session'
 import { cookies } from 'next/headers'
 
 // Specify protected and public routes
